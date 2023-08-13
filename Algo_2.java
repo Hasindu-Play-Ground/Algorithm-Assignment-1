76953b54efc9ad23a7fb78d3decdaba8fcbe9ced
Hasindu Dasanayaka <hasindudasanayaka@gmail.com>
2023-08-13 19:35:49 +0530

    Inter changes 1

--- Algo_2.java
+++ Algo_2.java
@@ -5,6 +5,7 @@ public class Algo_2 {
         int smallest = nums[0];
         int largest = nums[0];
         
+        
         int index = 1;
         do {
             if (nums[index] < smallest) {
@@ -20,5 +21,6 @@ public class Algo_2 {
         
         System.out.println("Smallest number: " + smallest);
         System.out.println("Largest number: " + largest);
+        
     }
 }
