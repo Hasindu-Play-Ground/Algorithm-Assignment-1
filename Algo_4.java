ca004372acbc362dcec13e0c80989e2ad58cbeea
Hasindu Dasanayaka <hasindudasanayaka@gmail.com>
2023-08-13 20:03:20 +0530

    Algo_4 Completed

--- Algo_4.java
+++ Algo_4.java
@@ -1,3 +1,25 @@
 public class Algo_4 {
     
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+        
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            // Swap elements at left and right indices
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            
+            // Move the pointers inward
+            left++;
+            right--;
+        }
+        
+        // Print out the elements in the nums array
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
 }
